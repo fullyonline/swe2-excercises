65be086809a2b2897fd1363488ff309a67d7b360
fullyonline <no.reply4me@gmail.com>
2022-03-03 20:51:55 +0100

    added getter and setter

--- src/main/java/ch/juventus/object/Address.java
+++ src/main/java/ch/juventus/object/Address.java
@@ -4,6 +4,39 @@ public class Address {
 
     private String street;
     private int number;
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
     private int zip;
     private String city;
 
--- src/main/java/ch/juventus/object/Person.java
+++ src/main/java/ch/juventus/object/Person.java
@@ -5,6 +5,47 @@ public class Person {
     private String firstname;
     private String lastname;
     private int age;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Boolean getHasPet() {
+        return hasPet;
+    }
+
+    public void setHasPet(Boolean hasPet) {
+        this.hasPet = hasPet;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
     private Boolean hasPet;
     private Address address;
 
