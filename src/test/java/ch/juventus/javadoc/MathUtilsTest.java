150ecbc06755a77149f9f41b73c987a7022fb659
fullyonline <no.reply4me@gmail.com>
2022-03-10 21:25:05 +0100

    split tests

--- src/test/java/ch/juventus/javadoc/MathUtilsTest.java
+++ src/test/java/ch/juventus/javadoc/MathUtilsTest.java
@@ -26,30 +26,46 @@ class MathUtilsTest {
         doubleList.add(3.0d);
         doubleList.add(4.0d);
 
-        assertEquals(8.0d, mathUtils.addition(doubleList));
+        Double result = mathUtils.addition(doubleList);
+
+        assertEquals(8.0d, result);
     }
 
     @Test
     void shouldAddSeveralNumbers() {
         List<Double> doubleList = new ArrayList<>();
 
-        doubleList.add(1.0d);
-        doubleList.add(-3.0d);
-        doubleList.add(4.0d);
+        doubleList.add(1.36666d);
+        doubleList.add(-3.1255d);
+        doubleList.add(4.4555d);
 
-        assertEquals(2.0d, mathUtils.addition(doubleList));
+        Double result = mathUtils.addition(doubleList);
+
+        assertEquals(2.6966599999999996d, result);
     }
 
     @Test
     void shouldSubtractPositiveNumbers() {
-        assertEquals(2, mathUtils.subtraction(4,2));
-        assertEquals(7, mathUtils.subtraction(10,3));
+        Double result = mathUtils.subtraction(4,2);
+        assertEquals(2, result);
     }
 
     @Test
-    void shouldSubtractPositiveAndNegativeNumbers() {
-        assertEquals(6, mathUtils.subtraction(4,-2));
-        assertEquals(-2, mathUtils.subtraction(10,12));
+    void shouldSubtractPositiveNumbersComplex() {
+        Double result = mathUtils.subtraction(10.2226,3.2444);
+        assertEquals(6.978199999999999, result);
+    }
+
+    @Test
+    void shouldSubtractNegativeNumbers() {
+        Double result = mathUtils.subtraction(4,-2);
+        assertEquals(6, result);
+    }
+
+    @Test
+    void shouldSubtractNegativeNumbersAndHasNegativeResult() {
+        Double result = mathUtils.subtraction(10,12);
+        assertEquals(-2, result);
     }
 
     @Test
@@ -63,8 +79,14 @@ class MathUtilsTest {
     }
 
     @Test
-    void shouldMultiply() {
-        assertEquals(12, mathUtils.multiplication(3, 4));
-        assertEquals(-12, mathUtils.multiplication(-3, 4));
+    void shouldMultiplyPositive() {
+        Double result = mathUtils.multiplication(3, 4);
+        assertEquals(12, result);
+    }
+
+    @Test
+    void shouldMultiplyNegative() {
+        Double result = mathUtils.multiplication(-3, 4);
+        assertEquals(-12, result);
     }
 }
