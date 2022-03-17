367b53bb634dc3cad1221b13e7440e50e0d7e858
fullyonline <no.reply4me@gmail.com>
2022-03-17 19:18:15 +0100

    added the Logger to the class Bank

--- src/main/java/ch/juventus/logging/Bank.java
+++ src/main/java/ch/juventus/logging/Bank.java
@@ -1,36 +1,54 @@
 package ch.juventus.logging;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Bank {
 
-    private Double money;
+    final Logger logger = LoggerFactory.getLogger(Bank.class);
+
+    private Double balance;
 
     public Bank(){
-        money = 0.d;
+        balance = 0.d;
     }
 
     public boolean withdraw(Double value){
-        Double newValue = money - value;
+        Double newBalance = balance - value;
 
-        if (value <= 0 || newValue < 0){
+        if (value <= 0 || newBalance < 0){
+            if(value <= 0){
+                logger.error("withdraw: The value to withdraw {} is negative", value);
+            }
+            else{
+                logger.error("withdraw: The new balance after withdrawing {} is negative", newBalance);
+            }
             return false;
         }
 
-        money = newValue;
+        logger.info("withdraw: old balace: {}, withdraw ammount: {}, new balance: {}", balance, value, newBalance);
+
+        balance = newBalance;
         return true;
     }
 
     public boolean deposit(Double value){
 
         if(value < 0){
+            logger.error("deposit: the deposit ammount {} is negative", value);
             return false;
         }
+        Double newBalance = balance + value;
 
-        money = money + value;
+        logger.info("deposit: old balace: {}, deposit ammount: {}, new balance: {}", balance, value, newBalance);
+
+        balance = newBalance;
         return true;
     }
 
     public Double retrieveBalance(){
-        return money;
+        logger.info("retrieveBalance: balace: {}", balance);
+        return balance;
     }
 
 }
